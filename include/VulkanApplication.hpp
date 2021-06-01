8dcffab4565e42aa7d9a904256f022054cf7c8ea
Zacharie Corniere <cornierez@gmail.com>
2021-06-01 12:54:35 +0200

    chore(vk_utils): vk_utils to a cpp file

--- include/VulkanApplication.hpp
+++ include/VulkanApplication.hpp
@@ -68,14 +68,3 @@ private:
 private:
     DeleteionQueue mainDeletionQueue;
 };
-
-struct VulkanException : public std::runtime_error {
-    VulkanException(const std::string message): std::runtime_error(message) {}
-    VulkanException(const VkResult er): std::runtime_error(vk_utils::tools::errorString(er)) {}
-};
-
-#define VK_TRY(x)                                \
-    {                                            \
-        VkResult err = x;                        \
-        if (err) { throw VulkanException(err); } \
-    }
--- include/vk_utils.hpp
+++ include/vk_utils.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+#include <stdexcept>
 #include <string>
 #include <vulkan/vulkan.h>
 
@@ -7,52 +9,18 @@ namespace vk_utils
 {
 namespace tools
 {
-    inline std::string errorString(VkResult errorCode)
-    {
-        switch (errorCode) {
-#define STR(r) \
-    case VK_##r: return #r
-            STR(NOT_READY);
-            STR(TIMEOUT);
-            STR(EVENT_SET);
-            STR(EVENT_RESET);
-            STR(INCOMPLETE);
-            STR(ERROR_OUT_OF_HOST_MEMORY);
-            STR(ERROR_OUT_OF_DEVICE_MEMORY);
-            STR(ERROR_INITIALIZATION_FAILED);
-            STR(ERROR_DEVICE_LOST);
-            STR(ERROR_MEMORY_MAP_FAILED);
-            STR(ERROR_LAYER_NOT_PRESENT);
-            STR(ERROR_EXTENSION_NOT_PRESENT);
-            STR(ERROR_FEATURE_NOT_PRESENT);
-            STR(ERROR_INCOMPATIBLE_DRIVER);
-            STR(ERROR_TOO_MANY_OBJECTS);
-            STR(ERROR_FORMAT_NOT_SUPPORTED);
-            STR(ERROR_SURFACE_LOST_KHR);
-            STR(ERROR_NATIVE_WINDOW_IN_USE_KHR);
-            STR(SUBOPTIMAL_KHR);
-            STR(ERROR_OUT_OF_DATE_KHR);
-            STR(ERROR_INCOMPATIBLE_DISPLAY_KHR);
-            STR(ERROR_VALIDATION_FAILED_EXT);
-            STR(ERROR_INVALID_SHADER_NV);
-#undef STR
-            default: return "UNKNOWN_ERROR";
-        }
-    }
-
-    inline std::string physicalDeviceTypeString(VkPhysicalDeviceType type)
-    {
-        switch (type) {
-#define STR(r) \
-    case VK_PHYSICAL_DEVICE_TYPE_##r: return #r
-            STR(OTHER);
-            STR(INTEGRATED_GPU);
-            STR(DISCRETE_GPU);
-            STR(VIRTUAL_GPU);
-            STR(CPU);
-#undef STR
-            default: return "UNKNOWN_DEVICE_TYPE";
-        }
-    }
+    std::string errorString(VkResult errorCode);
+    std::string physicalDeviceTypeString(VkPhysicalDeviceType type);
 }    // namespace tools
 }    // namespace vk_utils
+
+struct VulkanException : public std::runtime_error {
+    VulkanException(const std::string message): std::runtime_error(message) {}
+    VulkanException(const VkResult er): std::runtime_error(vk_utils::tools::errorString(er)) {}
+};
+
+#define VK_TRY(x)                                \
+    {                                            \
+        VkResult err = x;                        \
+        if (err) { throw VulkanException(err); } \
+    }
